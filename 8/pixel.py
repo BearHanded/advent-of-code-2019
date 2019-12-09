54e3b834d9415e40b01342f0266e42465af7f9d6
bmyers <bmyers@isaca.org>
2019-12-09 02:51:52 -0500

    so much for sleep

--- 8/pixel.py
+++ 8/pixel.py
@@ -4,16 +4,25 @@ fp = open('./input.txt', 'r')
 dimensions = 25, 6
 layer_size = dimensions[0] * dimensions[1]
 
-target = ''
-min_zero = 999999999999999
-
+msg = []
 for layer in iter(partial(fp.read, layer_size), ''):
+    msg.append(layer)
 
-    zero_count = layer.count('0')
+print(msg)
+focused_image = []
+for idx in range(0, layer_size):
+    focused_image.append(' ')
+    for level in range(0, len(msg)):
+        if msg[level][idx] != '2':
+            focused_image[idx] = msg[level][idx]
+            break
 
-    if zero_count < min_zero:
-        target = layer
-        min_zero = zero_count
+print(focused_image)
 
+rows = list(map(''.join, zip(*[iter(focused_image)]*25)))
+for row in rows:
+     print(row)
 
-print(target.count('1') * target.count('2'))
+print('----')
+for row in rows:
+     print(row.replace('0', ' '))
